40e6bd6e5bac5357243e26f89d9187eefe804e96
anilamurali <anilamurali98@gmail.com>
2022-05-24 17:47:04 +0530

    first coomit

---
+++ src/Component/Searchstudent.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const Searchstudent = () => {
+    var [admno,setAdmno]=useState("")
+    const setData=()=>{
+        var data={"admno":admno}
+        console.log(data)
+    }
+  return (
+    <div>
+        <div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div class="row g-3">
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label"> Admission Number</label>
+                    <input onChange={(e)=>{setAdmno(e.target.value)}} type="text" class="form-control"/>
+                </div>
+                
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button onClick={setData} class="btn btn-dark">SEARCH</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default Searchstudent
